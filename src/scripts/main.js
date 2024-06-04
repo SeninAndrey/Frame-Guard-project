8d22e63fe6c105416e32a6546e7f7c05f712ca04
SeninAndrey <senin85@gmail.com>
2024-06-04 20:19:18 +0300

    tablet, mobile and dark/light mode added 04.06

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,26 @@
 // ABOUT SLIDER
 
 const swiperAbout = new Swiper('.swiper__about', {
-    slidesPerView: 3,
-    spaceBetween: 20,
 
     // Optional parameters
     direction: 'horizontal',
     // loop: true,
     // allowTouchMove: false,
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 5,
+        },
+        500: {
+            slidesPerView: 2,
+            spaceBetween: 15,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        }
+    },
+
 });
 
 // PROJECTS SLIDER
@@ -29,4 +42,35 @@ const swiperProjects = new Swiper('.swiper__projects', {
 
 window.addEventListener("hashchange", function () {
     window.scrollTo(window.scrollX, window.scrollY - 50);
+});
+
+// THEME SWITCHER
+
+function initialTheme (themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;   
+    const darkModeIcon = document.querySelector('.mode__dark'); 
+    const lightModeIcon = document.querySelector('.mode__light')
+    if (themeName == 'light-mode') {
+        darkModeIcon.classList.add('mode--hidden');
+        lightModeIcon.classList.remove('mode--hidden');
+    } else {
+        darkModeIcon.classList.remove('mode--hidden');
+        lightModeIcon.classList.add('mode--hidden');
+    } 
+}
+
+function switchMode() {
+    if (localStorage.getItem('theme') == 'dark-mode') {
+        initialTheme('light-mode');
+    } else {
+        initialTheme('dark-mode');
+    }
+}  
+
+const modeButton = document.querySelector('.header__mode');
+modeButton.addEventListener('click', switchMode);
+
+window.addEventListener('DOMContentLoaded', () => {
+    initialTheme(localStorage.getItem('theme'));
 });
